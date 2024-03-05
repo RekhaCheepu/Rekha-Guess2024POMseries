77b850f2f7f14507344837f53082bef34693fb1c
Rekharani <122629816+RekhaCheepu@users.noreply.github.com>
2024-03-05 15:29:15 -0500

    Create Poc.java

---
+++ src/main/java/com/qa/guess/pages/Poc.java
@@ -0,0 +1,15 @@
+package com.qa.guess.pages;
+
+import org.openqa.selenium.By;
+
+public class PocPage {
+	 By poc= By.id("selenium");
+	 public PocPage() {
+		 
+	 }
+	 public void addToPoc() {
+		 System.out.println("poc is done");
+		 
+	 }
+
+}
