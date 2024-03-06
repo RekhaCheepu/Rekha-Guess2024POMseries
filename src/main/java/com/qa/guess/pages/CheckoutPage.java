ec09c762fe17d546822ef8660dcd335663ab46a9
Rekha <rekha.cheepu@gmail.com>
2024-03-05 22:39:22 -0500

    added test method

--- src/main/java/com/qa/guess/pages/CheckoutPage.java
+++ src/main/java/com/qa/guess/pages/CheckoutPage.java
@@ -3,13 +3,13 @@ package com.qa.guess.pages;
 public class CheckoutPage {
 	public void CheckOut() {
 		
-<<<<<<< HEAD
+
 		System.out.println("checkout --Rekha");
-=======
-		System.out.println("checkout --Sidhar");
->>>>>>> aae8ba907d028716b79e2e0f6ce725da7a58d9c0
-		
 		int i=100;
+		String name= "macbook";
+	}
+	public void test() {
+		System.out.println("testing");
 	}
 
 }
