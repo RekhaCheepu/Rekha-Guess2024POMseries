aae8ba907d028716b79e2e0f6ce725da7a58d9c0
Rekharani <122629816+RekhaCheepu@users.noreply.github.com>
2024-03-05 21:21:04 -0500

    Update CheckoutPage.java

--- src/main/java/com/qa/guess/pages/CheckoutPage.java
+++ src/main/java/com/qa/guess/pages/CheckoutPage.java
@@ -3,7 +3,7 @@ package com.qa.guess.pages;
 public class CheckoutPage {
 	public void CheckOut() {
 		
-		System.out.println("checkout --Rekha");
+		System.out.println("checkout --Sidhar");
 		
 		int i=100;
 	}
