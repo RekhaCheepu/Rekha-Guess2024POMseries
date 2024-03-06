7b7c02c6ea43bd458efd5889bdfbc3ff36d64849
Rekharani <122629816+RekhaCheepu@users.noreply.github.com>
2024-03-06 16:28:30 -0500

    Create SeleniumPage.java

---
+++ src/main/java/com/qa/guess/pages/SeleniumPage.java
@@ -0,0 +1,15 @@
+package com.qa.guess.pages;
+
+public class SeleniumPage {
+
+	public  void seleniumTest() {
+String s="tom";
+		
+			System.out.println("selenium testing");
+			
+			
+		}
+
+	}
+
+
